10441509dbce47df72e89b3ab7679c2c736a2e08
Karma akabane <Ahanaf-cell>
2025-08-02 15:45:48 +0600

    Refactor layout and page structure; update Navbar and globals.css

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <section className="pink_container">
+        <h1 className="heading">
+          Home <br />
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+         
+        </p>
+      </section>
+    </main>
+  );
+}
